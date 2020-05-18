052280f55003047e47d023e09fc1f1e49d1b59d1
mat-azvd <mat.azvd@gmail.com>
2020-05-18 17:40:01 -0300

    Reestruturacaophp

--- pagina-xbox.php
+++ pagina-xbox.php
@@ -5,7 +5,6 @@
 <link rel="stylesheet" type="text/css" href="css/slide.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="css/reset.css">
 
 <?php include("cabecalho.php");?>
 
